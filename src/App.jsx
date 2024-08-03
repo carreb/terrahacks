474ead3f5393cefc4264df5beff688835f3c0cd9
cyuber <alyboy2006@gmail.com>
2024-08-03 00:22:42 -0400

    added factors counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home.jsx';
+import Factors from './components/formulas/Factors.jsx';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' exact element={<Home/>}></Route>
-
+        <Route path='/factors' element={<Factors/>}></Route>
       </Routes>
     </Router>
     </>
