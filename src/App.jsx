c1e792e23091ebef0ff1777756cd785107920ce5
carreb <caleb@domyjobfor.me>
2024-08-02 22:14:46 -0400

    remove unneeded stuff from base

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
 import Home from './components/pages/Home.jsx';
-import SignUp from './components/pages/SignUp.jsx';
-import AboutMe from './components/pages/AboutMe.jsx';
-import Projects from './components/pages/Projects.jsx';
 
 function App() {
   return (
     <>
     <Router>
-      <Navbar/>
       <Routes>
         <Route path='/' exact element={<Home/>}></Route>
-        <Route path='/Home' exact element={<Home/>}></Route>
-        <Route path='/AboutMe' exact element={<AboutMe/>}></Route>
-        <Route path='/Projects' exact element={<Projects/>}></Route>
-        <Route path='/sign-up' exact element={<SignUp/>}></Route>
 
       </Routes>
     </Router>
