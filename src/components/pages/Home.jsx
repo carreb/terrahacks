6ac78aead3180532a125b77722494db2a80f317f
carreb <caleb@domyjobfor.me>
2024-08-03 04:45:39 -0400

    implement scroll lock animation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,27 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import '../../App.css';
 import '../homepage_styles.css';
 import GiantTimerSplash from '../GiantTimerSplash';
 import BloodDripSVG from '../BloodDripSVG.jsx'; // ignore error? it doesn't seem to mean anything
 import StickyTime from '../StickyTimer';
 
-
 const convertPxToVh = (px) => {
-    return px / window.innerHeight * 100;
-}
+  return (px / window.innerHeight) * 100;
+};
 
 const convertVhToPx = (vh) => {
-    return vh / 100 * window.innerHeight;
-}
+  return (vh / 100) * window.innerHeight;
+};
 
 function Home() {
-  const [scrollPosition, setScrollPosition] = useState(0);
+  const scrollPosition = useRef(0);
+  const lastScrollTime = useRef(0);
 
   const scrollSticky = convertVhToPx(49); // this variable controls when the sticky timer navbar thingie appears
 
   useEffect(() => {
     const handleScroll = () => {
-      setScrollPosition(window.scrollY);
+      if (window.scrollY < convertVhToPx(49)) {
+        // Check if the scroll direction is down
+        console.log('Window.scrollY = ' + window.scrollY);
+        console.log('ScrollPosition = ' + scrollPosition.current);
+
+        if (window.scrollY > scrollPosition.current) {
+          // if there was a scroll event too recently, don't do anything
+          if (Date.now() - lastScrollTime.current < 100) {
+            return;
+          }
+          window.scrollTo(0, convertVhToPx(100));
+          scrollPosition.current = window.scrollY;
+        } else {
+          scrollPosition.current = window.scrollY;
+        }
+      }
+      lastScrollTime.current = Date.now();
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -32,21 +48,19 @@ function Home() {
     };
   }, []);
 
-  return (
-    scrollPosition > scrollSticky ? (
-      <>
-        <div className='homepage-flex-container'>
-          <GiantTimerSplash />
-          <BloodDripSVG />
-        </div>
-        <StickyTime />
-      </>
-    ) : (
-      <div className='homepage-flex-container'>
+  return scrollPosition.current > scrollSticky ? (
+    <>
+      <div className="homepage-flex-container">
         <GiantTimerSplash />
         <BloodDripSVG />
       </div>
-    )
+      <StickyTime />
+    </>
+  ) : (
+    <div className="homepage-flex-container">
+      <GiantTimerSplash />
+      <BloodDripSVG />
+    </div>
   );
 }
 
