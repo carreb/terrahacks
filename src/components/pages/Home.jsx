305b4d096db54d7495e41d65a05f107203720a5f
carreb <caleb@domyjobfor.me>
2024-08-03 00:39:48 -0400

    Blood drip svg working

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import '../../App.css';
 import '../homepage_styles.css';
 import GiantTimerSplash from '../GiantTimerSplash';
+import BloodDripSVG from '../BloodDripSVG.jsx'; // ignore error? it doesn't seem to mean anything
 
 function Home () {
 
@@ -9,6 +10,7 @@ function Home () {
         <>
         <div className='homepage-flex-container'>
             <GiantTimerSplash />
+            <BloodDripSVG />
             <div className="padding"></div>
         </div>
         </>
