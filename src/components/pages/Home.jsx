896fdafc0cd20fd98792457864381e5361fc4514
carreb <caleb@domyjobfor.me>
2024-08-03 00:06:06 -0400

    working on homepage

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import '../../App.css';
-import '../timer.css';
+import '../homepage_styles.css';
 import GiantTimerSplash from '../GiantTimerSplash';
 
 function Home () {
 
     return (
         <>
+        <div className='homepage-flex-container'>
             <GiantTimerSplash />
+            <div className="padding"></div>
+        </div>
         </>
     );
 }
