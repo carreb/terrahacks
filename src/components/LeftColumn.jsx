7b653b7c403801381068a6d1721a470e9d9c31a6
carreb <caleb@domyjobfor.me>
2024-08-03 20:12:57 -0400

    change something idk

--- src/components/LeftColumn.jsx
+++ src/components/LeftColumn.jsx
@@ -5,8 +5,10 @@ const LeftColumn = () => {
     return (
         <div className="homepage-news-column">
             <div className="left-section">
-                <img src="https://via.placeholder.com/500x300" alt="" className="float-image-left" />
+                <h1>SIGMA SKIBIDI</h1>
+                <p>Toilet skibidi sigma Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, enim.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe laborum quasi quibusdam repellendus illum exercitationem similique sapiente. Error eius nulla adipisci nostrum laudantium, recusandae labore perspiciatis beatae, nemo vitae quibusdam vel inventore iste deserunt. Atque quia, dolorem similique error repellat nulla soluta ut nemo beatae aspernatur blanditiis distinctio, incidunt architecto, delectus dolorum quam ullam sint laboriosam eius praesentium officia deserunt veritatis corrupti reprehenderit. Quisquam sed dicta iusto impedit quod, adipisci eligendi unde consequuntur nesciunt! Earum unde hic magni neque. Nisi beatae officiis eaque ducimus ex, nam odio atque est adipisci inventore incidunt officia non ratione. Velit non necessitatibus tempora itaque.</p>
+                <img src="https://via.placeholder.com/500x300" alt="" className="float-image-left" />
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, obcaecati? Eveniet molestiae voluptates praesentium exercitationem nostrum eum illum rerum, sint facilis debitis perferendis dolores quae ut. Voluptates ratione beatae porro officiis, dolorum quaerat dignissimos iste unde blanditiis! Culpa velit ratione tenetur ullam omnis assumenda enim natus dignissimos tempora ad autem, molestias quaerat reiciendis excepturi quia dolor nulla vero est voluptate vitae non ea numquam nostrum! Nostrum fuga velit obcaecati, libero nihil recusandae sed harum consequuntur consequatur quis eius omnis vero eveniet corrupti ex veritatis corporis hic suscipit fugit? Assumenda dicta vero voluptas commodi, exercitationem dignissimos non maxime tempora voluptatem corporis!</p>
             </div>
         </div>
