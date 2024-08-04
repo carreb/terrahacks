e13d2c052d4e09234bfdebb8c53259b1672a68f8
carreb <caleb@domyjobfor.me>
2024-08-04 09:00:36 -0400

    REMOVE

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home.jsx';
 import Factors from './components/formulas/Factors.jsx';
-import Sim from './mapathon/Sim.jsx'
 import DeviceDoesNotMeetRequiredScreenSize from './components/pages/DeviceDoesNotMeetRequiredScreenSize.jsx';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Route path='/' exact element={<Home/>}></Route>
         <Route path='/factors' element={<Factors/>}></Route>
         <Route path='/device-does-not-meet-required-screen-size' element={<DeviceDoesNotMeetRequiredScreenSize />}></Route>
-        <Route path='/Sim' exact element={<Sim/>}> </Route>
       </Routes>
     </Router>
     </>
