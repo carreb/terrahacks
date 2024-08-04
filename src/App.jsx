0df8dece9daf13f0c1f33a5b3d9737fb62e7c08d
Zenkqi <SSGSSAchita@gmail.com>
2024-08-04 08:40:00 -0400

    route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home.jsx';
 import Factors from './components/formulas/Factors.jsx';
+import Sim from './mapathon/Sim.jsx'
 import DeviceDoesNotMeetRequiredScreenSize from './components/pages/DeviceDoesNotMeetRequiredScreenSize.jsx';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' exact element={<Home/>}></Route>
         <Route path='/factors' element={<Factors/>}></Route>
         <Route path='/device-does-not-meet-required-screen-size' element={<DeviceDoesNotMeetRequiredScreenSize />}></Route>
+        <Route path='/Sim' exact element={<Sim/>}> </Route>
       </Routes>
     </Router>
     </>
