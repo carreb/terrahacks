a3bafc1a28061b224c1f17ed1145c5433abce898
carreb <caleb@domyjobfor.me>
2024-08-04 06:59:50 -0400

    images fixes

--- src/components/LeftColumn.jsx
+++ src/components/LeftColumn.jsx
@@ -6,6 +6,7 @@ const LeftColumn = () => {
         <div className="homepage-news-column">
             <div className="left-section">
                 <h1 className="smaller-title">GREENHOUSE GASES</h1>
+                <img src="../public/images/graph_gases.png" alt="" className="float-image-left" />
                 <p>
                     Greenhouse gases are by far the greatest contributors of climate change.
                     To stop climate change, greenhouse gas emissions need to be reduced to net zero worldwide.
@@ -14,9 +15,9 @@ const LeftColumn = () => {
                     Since pre industrial times, net carbon dioxide concentrations have increased by 40%, with most of increase occuring in the 21st and late 20th centrury.
                     Major common sources include burning coal and other fossil fuels and cutting down forests.
                 </p>
-                <img src=".../public/images/graph_gases.png" alt="" className="float-image-left" />
             </div>
             <div className="right-section">
+                <img src="../public/images/graph_1.png" alt="" className="float-image-right" />
                 <p>
                     Methane is the second most generated greenhouse gas, where modern concentrations are 2.5x more than their pre industrial concentrations.
                     The primary souces of emission of methane gas are from dairy farms and from melting glaciers and ice caps.
@@ -25,7 +26,6 @@ const LeftColumn = () => {
                     The least commonly understood cause of greenhouse gases come from the various household products that release fluorinated gases.
                     Fluorinated gases have a 23000 times more potent warming effect than carbon dioxide, and can be released from fridges, air conditioners, insulation, and electrical distribution.
                 </p>
-                <img src=".../public/images/graph_1.png" alt="" className="float-image-right" />
             </div>
         </div>
     )
