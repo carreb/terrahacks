c58e645d29e1445478bc5bc3a6fb3be824db904f
cyuber <alyboy2006@gmail.com>
2024-08-04 05:36:49 -0400

    finished article

--- src/components/RightColumn.jsx
+++ src/components/RightColumn.jsx
@@ -21,7 +21,12 @@ const LeftColumn = () => {
                 <p>
                     A many times overstated contributor of climate change is natural causes.
                     In fact, natural causes, ranging from variable solar strength to volcanic erruptions have only accounted for a difference in 0.1 degrees Celsius in total warming from 1890 to 2010.
-                    Some say that solar variations in 
+                    Some say that solar variations have had the most effect on temperature changes, but satellites have been observing the sun's temperature since 1978 and have found little significant deviation.
+                    Natural climate change was significantly more prevalent before the industrial revolution.
+                    Most ice ages in human history can be accounted for by the slight changes in the axis of earth's rotation and its orbit, but that is a negligible factor in modern times and doesn't explain the patterns we experience today.
+                    A paradoxical fact is that volcanic activity actually decrease global temperature.
+                    Even though they are expelling particles such as CO2 into the atmosphere, they are suspended for much shorter times than greenhouse gases.
+                    More importantly, they act as large clouds of reflectors of sunlight.
                 </p>
                 <img src="https://via.placeholder.com/500x300" alt="" className="float-image-right" />
             </div>
