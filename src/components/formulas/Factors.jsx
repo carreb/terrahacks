e3565fa64e73113b6dcad2149d9742dbc73ceff5
cyuber <alyboy2006@gmail.com>
2024-08-04 03:55:25 -0400

    factors refactor

--- src/components/formulas/Factors.jsx
+++ src/components/formulas/Factors.jsx
@@ -5,7 +5,7 @@ const startTime = new Date().getTime();
 function Factors() {
 
     // Energy sources
-    const [nonRenewable, setNonRenewable] = useState(0);
+    const [nonRenewable, setNonRenewable] = useState(2);
     const [renewable, setRenewable] = useState(0);
 
     // Agriculture
@@ -21,6 +21,7 @@ function Factors() {
     const [marineFaunaPop, setMarineFaunaPop] = useState(100);
 
     // Energy quota
+    const [energy, setEnergy] = useState(0);
     const [energyQuota, setEnergyQuota] = useState(0);
 
     // Temperature change
@@ -36,7 +37,7 @@ function Factors() {
     let K_human = 1
     let K_energyQuota = (energyQuota > humanPop * 1.5) ? (0) : ((humanPop * 1.5 - energyQuota)/100)
     let K_energy = 2
-    let K_T = 3 - (landFloraPop + marineFloraPop)/100
+    let K_T = 3 - (landFloraPop + marineFloraPop)/100 + temperature / 100
     let K_landFauna = 5
     let K_hunting = hunting / 50
     let K_landFlora = 5
@@ -46,6 +47,9 @@ function Factors() {
     let K_marineFlora = 5
     let K_marineFaunaPop = landFaunaPop / 1000  
     let K_Temp = 2
+    let K_landFloraPop = landFloraPop / 1000
+    let K_marineFloraPop = marineFloraPop / 1000
+    let K_nonRenewable = nonRenewable / 20
 
     let timeElapsed = (start) => {
         const currentTime = new Date().getTime();
@@ -56,13 +60,14 @@ function Factors() {
 
     let update = () => {
         // Relationships
-        setHumanPop(Math.round(K_food-K_human-K_energyQuota)*humanPop);
-        setLandFaunaPop(Math.round(K_landFauna-K_hunting-K_T)*landFaunaPop);
-        setLandFloraPop(Math.round(K_landFlora-K_landFaunaPop-K_T)*landFloraPop);
-        setMarineFaunaPop(Math.round(K_marineFauna-K_fishing-K_T)*landFaunaPop);
-        setMarineFloraPop(Math.round(K_marineFlora-K_marineFaunaPop-K_T)*landFloraPop);
-        setEnergyQuota(Math.round(humanPop*K_energy))
-        setTemperature(K_Temp-K_marineFaunaPop-K_marineFloraPop)
+        setEnergy(Math.round(20*nonRenewable+10*renewable))
+        setEnergyQuota(Math.round(humanPop+K_energy))
+        setHumanPop(Math.round(K_food-K_human-K_energyQuota)+humanPop);
+        setLandFaunaPop(Math.round(K_landFauna-K_hunting-K_T)+landFaunaPop);
+        setLandFloraPop(Math.round(K_landFlora-K_landFaunaPop-K_T)+landFloraPop);
+        setMarineFaunaPop(Math.round(K_marineFauna-K_fishing-K_T)+landFaunaPop);
+        setMarineFloraPop(Math.round(K_marineFlora-K_marineFaunaPop-K_T)+landFloraPop);
+        setTemperature(K_Temp-K_landFloraPop-K_marineFloraPop+K_nonRenewable)
     }
 
     useEffect(() => {
