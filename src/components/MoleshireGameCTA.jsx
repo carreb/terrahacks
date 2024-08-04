e7f746cd86016537e2eba00fd97406d3f15ace4b
carreb <caleb@domyjobfor.me>
2024-08-04 09:03:32 -0400

    gnjghm jh

--- src/components/MoleshireGameCTA.jsx
+++ src/components/MoleshireGameCTA.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import './moleshire_cta.css';
 
 const MoleshireGameCTA = () => {
 
-    const navigate = useNavigate();
+    const openGameTab = () => {
+        window.open('https://doomsday.sim.domyjobfor.me', '__blank');
+    }
 
     return (
         <div className="moleshire-cta">
             <h1 className="smaller-title">Play a Live Simulation</h1>
             <p className="large-subtitle">Take control over the fictional island of Moleshire and try to prevent climate change from eradicating civilization and animal populations, along with keeping society from collapsing! Experience exactly why it's so hard to create action with regards to Climate Change issues.</p>
-            <button>PLAY NOW!</button>
+            <button onClick={openGameTab}>PLAY NOW!</button>
         </div>
     )
 }
