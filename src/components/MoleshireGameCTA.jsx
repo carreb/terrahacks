906229fbb06b367b033f6c4520459e63d195a46a
carreb <caleb@domyjobfor.me>
2024-08-04 08:55:04 -0400

    remove mapathon

--- src/components/MoleshireGameCTA.jsx
+++ src/components/MoleshireGameCTA.jsx
@@ -10,7 +10,7 @@ const MoleshireGameCTA = () => {
         <div className="moleshire-cta">
             <h1 className="smaller-title">Play a Live Simulation</h1>
             <p className="large-subtitle">Take control over the fictional island of Moleshire and try to prevent climate change from eradicating civilization and animal populations, along with keeping society from collapsing! Experience exactly why it's so hard to create action with regards to Climate Change issues.</p>
-            <button onClick={navigate("/Sim")}>PLAY NOW!</button>
+            <button>PLAY NOW!</button>
         </div>
     )
 }
